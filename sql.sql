3a9d33ff60a235c3ecd458034d107456cb76d0f5
Priya Gawhane <108482615+Priya-gawhane@users.noreply.github.com>
2025-04-12 22:37:18 +0530

    LIBRARY DATABASE

--- sql.sql
+++ sql.sql
@@ -1,19 +1,9 @@
-CREATE DATABASE library;
-USE library;
+create database library_demo;
 
-CREATE TABLE Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    genre VARCHAR(100),
-    published_year YEAR,
-    is_available BOOLEAN DEFAULT TRUE
-);
+drop database library;
 
-CREATE TABLE Members (
-    member_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255),
-    phone_number VARCHAR(15),
-    join_date DATE DEFAULT (CURRENT_DATE)
-);
+CREATE TABLE readers(
+    reader_id VARCHAR(6),
+    fname VARCHAR(59),
+    pname VARCHAR(50)
+)
