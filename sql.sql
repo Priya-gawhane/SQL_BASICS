88d7120cb1cc33ec18331ceb8cf910f68765d840
Priya Gawhane <108482615+Priya-gawhane@users.noreply.github.com>
2025-04-13 23:41:41 +0530

    Add files via upload

--- sql.sql
+++ sql.sql
@@ -1,9 +1,3 @@
 create database library_demo;
 
-drop database library;
-
-CREATE TABLE readers(
-    reader_id VARCHAR(6),
-    fname VARCHAR(59),
-    pname VARCHAR(50)
-)
+drop database library_demo;
